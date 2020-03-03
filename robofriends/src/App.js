8a0d793329f3d3f940e135d83f839ec3a00797b5
harishankarsivaji <harishs92@gmail.com>
2020-03-03 17:05:00 +0000

    used hooks

--- robofriends/src/App.js
+++ robofriends/src/App.js
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, { useCallback, useState} from 'react';
 import {robots} from './robots';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
 
 const App = () => {
+
+    const [searchField, setSearchField] =useState('');
+
+    const handleChange = useCallback ((event) => { 
+        setSearchField (event.target.value);       
+    });
+
+    const filteredRobot = useCallback ( robots.filter(robots => {
+        return robots.name.toLowerCase().includes(searchField.toLowerCase());
+    }));
+    // console.log(filteredRobot);
+
     return (
-    <div className='tc'>
-        <h1>Robo Friends</h1>
-        <SearchBox />
-        <CardList robots={robots} />
-    </div>
+        <div className='tc'>
+            <h1> --- Hello Robots --- </h1>
+            <SearchBox searchChange={handleChange}/>
+            <CardList robots={filteredRobot} />
+        </div>
     )
 }
 
